aa2356eb2af852f23d8c646e5dc8ee31d1acad40
Your Name <you@example.com>
2019-05-22 17:19:42 +0800

    s

--- app/Api/Controllers/RunController.php
+++ app/Api/Controllers/RunController.php
@@ -3,14 +3,33 @@
 namespace App\Api\Controllers;
 
 use App\Keyword;
+use App\Renwu;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class RunController extends Controller
 {
     public function remote()
     {
-        $keyword = Keyword::where(['status'=>1])->get()->toJson();
-        return $keyword;
+        $time = date('Y-m-d');
+        $data = Renwu::where('created_at','like','%'.$time.'%')->where(['status'=>0])->first();
+        if (empty($data)){
+            $today = Keyword::where(['status'=>1])->select('dohost','keyword','click','xzh')->get()->toArray();
+            foreach ($today as $key=>$item){
+                $today[$key]['created_at'] =  date('Y-m-d H:i:s');
+                $today[$key]['updated_at'] =  date('Y-m-d H:i:s');
+             }
+            DB::table('renwu')->insert($today);
+        }
+        $renwu = Renwu::where('created_at','like','%'.$time.'%')->where(['status'=>0])->first();
+        if ($renwu->click == $renwu->has_click){
+            $renwu->status = 1;
+            $renwu->save();
+        }
+        $one = Renwu::where('created_at','like','%'.$time.'%')->where(['status'=>0])->first();
+        $one->has_click = $one->has_click +1;
+        $one->save();
+        return $one;
     }
 }
---
+++ app/Renwu.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Renwu extends Model
+{
+    protected $table ="renwu";
+}
